426332156c5620e57463e5ac1fd94ee481e8dc03
Adrian Posada <adrian-924@hotmail.com>
2022-07-07 16:47:20 -0500

    ciclos request y response

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, request
+from flask import Flask, make_response, redirect, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    user_ip = request.remote_addr     #----- > Regresa la ip que se obtiene desde la peticion del browser
-    return f"Hello World Flask, tu ip es {user_ip}"
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect("/hello"))     #-----> Make response redirige al usuario al path hello, se guarda en una variable
+    response.set_cookie("user_ip", user_ip)     #-----> Pone una cookie que es igual a la ip del usuario y se va a llamar user_ip
+
+    return response
+
+
+@app.route("/hello")
+def hello():
+    user_ip = request.cookies.get("user_ip")     #-----> Obtenemos la ip del usuario desde las cookies del browser y no directamente de las request
+
+    return f"Hello World!!! Tu ip es {user_ip}"
