09ca24654e128ab72f3498c19ec9327ad834dcdb
Adrian Posada <adrian-924@hotmail.com>
2022-07-11 17:45:17 -0500

    clase de manejo de errores

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, make_response, redirect, request, render_template
+from os import abort
+from flask import Flask, make_response, redirect, request, render_template, abort
 
 app = Flask(__name__)
 
 todos = ["Comprar Cafe", "Enviar solicitud de comrpra", "Enviar video al productor"]
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
+@app.route("/500")
+def error500():
+    abort(500)
+    
+ 
 @app.route("/")
 def index():
     user_ip = request.remote_addr
