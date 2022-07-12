724bc9c823610d9f6184a3b64baddd147422f912
Adrian Posada <adrian-924@hotmail.com>
2022-07-12 10:02:52 -0500

    clase de configuracion de flask

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from ensurepip import bootstrap
-from os import abort
-from flask import Flask, make_response, redirect, request, render_template, abort
+from flask import Flask, make_response, redirect, request, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ["Comprar Cafe", "Enviar solicitud de comrpra", "Enviar video al productor"]
 
@@ -16,28 +16,22 @@ def not_found(error):
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('500.html', error=error)
 
 
-@app.route("/500")
-def error500():
-    abort(500)
-
- 
 @app.route("/")
 def index():
     user_ip = request.remote_addr
-    response = make_response(redirect("/hello"))     #-----> Make response redirige al usuario al path hello, se guarda en una variable
-    response.set_cookie("user_ip", user_ip)     #-----> Pone una cookie que es igual a la ip del usuario y se va a llamar user_ip
-
+    response = make_response(redirect('/hello'))     
+    session["user_ip"] = user_ip
     return response
 
 
-@app.route("/hello")
+@app.route('/hello')
 def hello():
-    user_ip = request.cookies.get("user_ip")     #-----> Obtenemos la ip del usuario desde las cookies del browser y no directamente de las request
+    user_ip = session.get('user_ip')    
     context = {
-        "user_ip": user_ip,
-        "todos": todos,
+        'user_ip': user_ip,
+        'todos': todos,
     }
-    return render_template("hello.html", **context)     #-----> Renderea un template en un html 
+    return render_template('hello.html', **context)     
